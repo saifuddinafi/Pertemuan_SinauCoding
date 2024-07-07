273be8b22c271fcdb4a66fbaf7dda121a10b0aef
Saifuddinafi <afi.saifudin7@gmail.com>
2024-07-08 02:37:32 +0700

    revisi style

--- Pertemuan7_PengenalanVueJS_SinauCoding/src/App.vue
+++ Pertemuan7_PengenalanVueJS_SinauCoding/src/App.vue
@@ -1,85 +1,46 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import ilustratorImage from './assets/Ilustrator.svg'
+import twitterImage from './assets/Logo Twitter.svg'
+import googleImage from './assets/Logo Google.svg'
+import appleImage from './assets/Logo Apple.svg'
 </script>
 
+
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  
+    <div class="left-side">
+        <img :src="twitterImage" alt="Twitter Logo" class="logo">
+        <h1>Welcome to Twitter</h1>
+        <p>Happening now, join twitter today</p>
+        <img :src="ilustratorImage" alt="Illustration" class="illustration">
     </div>
-  </header>
-
-  <RouterView />
+    <div class="right-side">
+        <div class="header">
+            <img :src="twitterImage" alt="Twitter Logo" class="logo">
+            <div class="sign-up">
+                Don't have an account? <a href="./index.html">Sign Up</a>
+            </div>
+        </div>
+        <h1 class="login">Log in to Twitter</h1>
+        <p class="login-twitter">Enter Twitter Account</p>
+        <label class="input-textpass">
+            <input type="text" placeholder="Phone, email or username" name="username" id="username" autocomplete="username">
+            <input type="password" placeholder="Password" name="password" id="password" autocomplete="current-password">
+        </label>
+        <div class="forgot-password">
+            <a href="./index.html">Forgot password?</a>
+        </div>
+        <a href="./index.html" >
+            <button class="sign-in">Sign In</button>
+        </a>
+        <div class="or">or</div>
+        <div class="social-login">
+            <a href="https://accounts.google.com/login?hl=in" >
+                <button class="sign-google"><img :src="googleImage" alt="Google Logo" width="20px" height="20px">Sign in with Google</button>
+            </a>
+            <a href="https://support.apple.com/id-id/111001" >
+                <button class="sign-apple"><img :src="appleImage" width="20px" height="20px" alt="Apple Logo">Sign in with Apple</button>
+            </a>
+        </div>
+    </div>  
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
